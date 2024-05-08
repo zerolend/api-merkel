5704e536e14118290eec724a1b36b1e8a4daaebb
Ghost Scripter <ghostscripter@MacBook-Pro-2.local>
2024-05-08 12:05:27 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as http from "http";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import {Router} from "express"
+import nconf from "nconf";
+import { getProofs } from "./merkleProof";
+
+const app = express();
+const server = new http.Server(app);
+const router = Router();
+
+router.get("/", (_req, res) => {
+    res.json({
+      uptime: process.uptime(),
+      online: true,
+      message: "fuck off",
+    });
+  });
+
+router.get("/merkleProof",getProofs );
+
+app.use(router)
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.set("port", nconf.get("PORT") || 5002);
+
+const port = app.get("port");
+server.listen(port, () => console.log(`server started on port ${port}`));
---
+++ src/merkleProof.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { ethers } from "ethers";
+import axios from "axios";
+
+interface AddressData {
+  address: string;
+  totalAmount: string;
+  proofs: string[];
+}
+
+let addressData: AddressData[];
+
+const getDataForAddress = (walletAddress: string) => {
+  return addressData.find(
+    (e) => e.address.toLowerCase() === walletAddress.toLowerCase()
+  );
+};
+
+export const getProofs = async (req: Request, res: Response) => {
+  const walletAddress: string = req.query.walletAddress as string;
+  console.log(walletAddress);
+  
+  if (walletAddress && ethers.isAddress(walletAddress)) {
+    if (addressData) {
+      const data = getDataForAddress(walletAddress);
+      if (data) {
+        res.json(data);
+        console.log('fetched from cahce');
+        
+      } else {
+        res.json(`data for address ${walletAddress} not found`);
+      }
+    } else {
+      try {
+        const response = await axios.get(
+          "https://raw.githubusercontent.com/deadshotryker/merkle/master/v1.json"
+        );
+        addressData = response.data.addressData;
+        const data = getDataForAddress(walletAddress);
+        if (data) {
+          res.json(data);
+        console.log('fetched from url');
+        } else {
+          res.json(`data for address ${walletAddress} not found`);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        //   throw error;
+      }
+    }
+  }
+};
