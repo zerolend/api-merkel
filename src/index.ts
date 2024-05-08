21ea495019ab4d1768c49fe5952daaba4f2f04e7
Ghost Scripter <ghostscripter@zerolend.xyz>
2024-05-08 17:44:11 +0530

    minor change

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import * as http from "http";
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import {Router} from "express"
 import nconf from "nconf";
 import { getProofs } from "./controller/merkleProof";
 
 const app = express();
 const server = new http.Server(app);
-const router = Router();
 
-router.get("/", (_req, res) => {
+app.get("/", (_req, res) => {
     res.json({
       uptime: process.uptime(),
       online: true,
@@ -18,15 +16,14 @@ router.get("/", (_req, res) => {
     });
   });
 
-router.get("/merkleProof",getProofs );
-
-app.use(router)
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+  
+app.get("/merkleProof",getProofs );
 
 app.set("port", nconf.get("PORT") || 5002);
 
 const port = app.get("port");
+
 server.listen(port, () => console.log(`server started on port ${port}`));
